de29fcbea359d29060d72c09ef17101cf337366e
Ibrohim Abdulfattah <ibrohim97uz@gmail.com>
2021-08-06 21:30:09 +0500

    added new website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
 var menu = document.querySelector(".burger_menu");
 var list = document.querySelector(".nav_list_burger");
+var toTop = document.querySelector(".to_top");
+
 menu.addEventListener("click", function () {
   menu.classList.toggle("transform");
   list.classList.toggle("show");
 });
-
+window.addEventListener("scroll", function () {
+  var yonalish = window.scrollY;
+  if (yonalish == 400 || yonalish > 400) {
+    toTop.style.display = "flex";
+  } else {
+    toTop.style.display = "none";
+  }
+});
 //create function for get random color
-
-
-
-
